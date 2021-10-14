7df3df85ee9f133ae7e74d4ca1cbdd583f9bf3c4
Punnaphat Phiamtanapuwanon <punnaphat.zaza@gmail.com>
2021-10-14 19:50:49 +0700

    Horizontal Scrolling
    Update Horizontal Scrolling

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var elmnt = document.getElementById("scroll");
+  var x = elmnt.scrollLeft;
+  var y = elmnt.scrollTop;
+  console.log(y);
+  distances.innerHTML = y;
+  // Message
+
+}
