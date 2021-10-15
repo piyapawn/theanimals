3b23d4aed63cb2cbf4c8c1affa497efe2c04033b
Punnaphat Phiamtanapuwanon <punnaphat.zaza@gmail.com>
2021-10-15 22:03:26 +0700

    prototype webpage
    update prototype webpage

--- javascript.js
+++ javascript.js
@@ -3,7 +3,14 @@ function myFunction() {
   var x = elmnt.scrollLeft;
   var y = elmnt.scrollTop;
   console.log(y);
-  scrollNumber.innerHTML = y;
-  // Message
+  // scrollNumber.innerHTML = y;
+
+  // Jeep Car
+  if(y>=1500){
+    document.querySelector("#jeep").style.left = "105vw";
+  }
+  else{
+    document.querySelector("#jeep").style.left = "50vw";
+  }
   
 }
